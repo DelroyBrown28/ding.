18e1f7bfe1bb9f8a658703ff1e33796182bb70c7
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-05-25 17:01:52 +0100

    added innerhtml

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
 
-    
+    $('.choice_car').click( function () {
+        document.getElementById('car_text').innerHTML = "Great, got it.";
+    })
+
+    $('.choice_van').click( function () {
+        document.getElementById('van_text').innerHTML = "Great, got it.";
+    })
+
+    $('.choice_bike').click( function () {
+        document.getElementById('bike_text').innerHTML = "Great, got it.";
+    })
+
 
 
 });
