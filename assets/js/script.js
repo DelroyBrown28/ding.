a4196275e4aed34230fbcd34ddbe26a9555539fa
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-05-25 20:20:48 +0100

    Updated design of main page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,16 @@
 $(document).ready(function () {
 
-    $('.choice_car').click( function () {
+    $('.choice_car').click(function () {
         document.getElementById('car_text').innerHTML = "Great, got it.";
     })
 
-    $('.choice_van').click( function () {
+    $('.choice_van').click(function () {
         document.getElementById('van_text').innerHTML = "Great, got it.";
     })
 
-    $('.choice_bike').click( function () {
+    $('.choice_bike').click(function () {
         document.getElementById('bike_text').innerHTML = "Great, got it.";
     })
 
 
-
 });
-
